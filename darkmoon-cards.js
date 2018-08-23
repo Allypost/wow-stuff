5d2e7da109e9ad0af77718fea4347d612139c74d
Josip Igrec <me@allypost.net>
2018-08-23 20:26:46 +0200

    Revamped darkmoon card calculator

--- darkmoon-cards.js
+++ darkmoon-cards.js
@@ -1,43 +1,16 @@
-const request = require('request-promise');
+const http = require('http');
 const moment = require('moment');
+const request = require('request-promise');
 const formatText = require('string-kit').format;
-
-const decks = {
-    // Fathoms
-    159125: [
-        153605, 153621, 153622, 153623, 153624, 153625, 153626, 153627,
-    ],
-    // Squalls
-    159126: [
-        153604, 153614, 153615, 153616, 153617, 153618, 153619, 153620,
-    ],
-    // Tides
-    159127: [
-        153603, 153607, 153608, 153609, 153610, 153611, 153612, 153613,
-    ],
-    // Blockades
-    159128: [
-        153606, 153628, 153629, 153630, 153631, 153632, 153633, 153634,
-    ],
-};
-
-async function getItemData(itemId) {
-    const uri = `https://eu.api.battle.net/wow/item/${itemId}?locale=en_GB&apikey=ajb6j8226ywqrt2mx6npqyut57czggau`;
-
-    return request(
-        {
-            uri,
-            json: true,
-            headers: {
-                'Accept': 'application/json',
-                'User-Agent': 'Allypost Profit Bot',
-            },
-        },
-    );
-}
 
 function moveUpLines(numLines = 1) {
     process.stdout.write(`\x1b[${numLines}F`);
+}
+
+function moveTo(x = 1, y = 1) {
+    [ x, y ] = [ x, y ].map((x) => Math.max(1, x));
+
+    process.stdout.write(`\x1b[${x};${y}H`);
 }
 
 function clearLine() {
@@ -56,17 +29,62 @@ function log(...args) {
     console.log(fmt`^-|>^ `, ...args);
 }
 
-function toHHMMSS(secs) {
-    const sec_num = parseInt(secs, 10);
-    const hours = Math.floor(sec_num / 3600) % 24;
-    const minutes = Math.floor(sec_num / 60) % 60;
-    const seconds = sec_num % 60;
-    const timeNames = [ 'h', 'm', 's' ];
+async function getItemData(itemId) {
+    const uri = `https://eu.api.battle.net/wow/item/${itemId}?locale=en_GB&apikey=ajb6j8226ywqrt2mx6npqyut57czggau`;
 
-    return [ hours, minutes, seconds ]
-        .map((v, i) => `${v}${timeNames[ i ]}`)
-        .filter(t => t.substr(0, 1) !== '0')
-        .join(' ');
+    return request(
+        {
+            uri,
+            json: true,
+            headers: {
+                'Accept': 'application/json',
+                'User-Agent': 'Allypost Profit Bot',
+            },
+        },
+    );
+}
+
+async function getAuctionUrl() {
+    const { url, lastModified: date } =
+        await request({ uri: 'https://eu.api.battle.net/wow/auction/data/draenor?locale=en_GB&apikey=ajb6j8226ywqrt2mx6npqyut57czggau', json: true })
+            .then(({ files }) => files.sort((a, b) => b.lastModified - a.lastModified).pop());
+
+    if (!url)
+        return { url: '', date: 0 };
+
+    return { url, date };
+}
+
+async function fetchAuctionData(url, tick = () => 0) {
+    if (!url)
+        return null;
+
+    let auctionData = '';
+
+    return new Promise((resolve, reject) => {
+        http.get(url, (res) => {
+            const totalLength = Number(res.headers[ 'content-length' ] || Number.MAX_SAFE_INTEGER);
+
+            res.on('data', (chunk) => {
+                auctionData += chunk;
+
+                tick(auctionData.length / totalLength);
+            });
+
+            res.on('end', () => {
+                try {
+                    const { auctions } = JSON.parse(auctionData);
+                    resolve(auctions);
+                } catch (e) {
+                    reject(null);
+                }
+            });
+
+            res.on('error', () => {
+                reject(null);
+            });
+        });
+    });
 }
 
 function parseAuctionData(auctions = [], deckId, deckCards) {
@@ -105,18 +123,59 @@ function parseAuctionData(auctions = [], deckId, deckCards) {
     return { deckAuction, cardAuctions };
 }
 
-function loadingBar(timeout) {
-    const then = Date.now();
-    clearLine();
-    process.stdout.write(fmt`Waiting for ^+${toHHMMSS(Math.ceil(timeout / 1000))} ${timeout % 1000}ms^`);
+async function formatAuctionData(auctions, [ deckId, deckCards ]) {
+    const { deckAuction, cardAuctions } = parseAuctionData(auctions, deckId, deckCards);
 
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            const diff = Date.now() - then;
+    const deckCost = Math.ceil(deckAuction.buyout);
+    const cardCost = cardAuctions.reduce((s, c) => s + c, 0);
+    const profit = Math.floor((deckCost - cardCost) * 0.95);
 
-            resolve(timeout - diff);
-        }, 60);
-    });
+    const deckData = await getItemData(deckId);
+    const profitPercent = Math.floor(profit / cardCost * 100);
+
+    return {
+        name: deckData.name,
+        deck: deckCost,
+        cost: cardCost,
+        profit: profit,
+        profitPercent: Math.abs(profitPercent),
+    };
+}
+
+function toHHMMSS(secs) {
+    const s = Math.round(secs);
+    const hours = Math.floor(s / 3600) % 24;
+    const minutes = Math.floor(s / 60) % 60;
+    const seconds = s % 60;
+    const timeNames = [ 'h', 'm', 's' ];
+
+    return [ hours, minutes, seconds ]
+               .map((v, i) => `${v}${timeNames[ i ]}`)
+               .filter(t => t.substr(0, 1) !== '0')
+               .join(' ') || '0s';
+}
+
+function getProgressBar(text) {
+    const startTime = Date.now();
+
+    function eta(percent) {
+        const rate = (Date.now() - startTime) / percent / 1000;
+        const seconds = Math.round((1 - percent) * rate);
+
+        return toHHMMSS(seconds);
+    }
+
+    function percent(t) {
+        t *= 100;
+        return `${t.toFixed(2)}%`;
+    }
+
+    function update(percentage = 0.0) {
+        log(`${text} | ${percent(percentage)} | ETA ${eta(percentage)}`);
+        moveUpLines();
+    }
+
+    return { update: (percent) => update(percent) };
 }
 
 function getDeckNameColour(deck) {
@@ -141,75 +200,9 @@ function getDeckNameColour(deck) {
     return '^g';
 }
 
-async function doWork(timeout = 0) {
-    if (timeout > 0)
-        return loadingBar(timeout)
-            .then((remaining) => doWork(remaining));
-
-    const initCallTime = Date.now();
-
-    clearLine();
-    process.stdout.write('\x1b[1;1H');
-    log('Fetching auction data locations...');
-    log('');
-    log('');
-    moveUpLines(3);
-
-    const { url, lastModified: latestFileModified } =
-        await request({ uri: 'https://eu.api.battle.net/wow/auction/data/draenor?locale=en_GB&apikey=ajb6j8226ywqrt2mx6npqyut57czggau', json: true })
-            .then(({ files }) => files.sort((a, b) => b.lastModified - a.lastModified).pop());
-
-    if (!url)
-        return;
-
-    log(fmt`Latest auction data is ^+${toHHMMSS((moment().utc() - latestFileModified) / 1000)} old^`);
-
-    const startTimeFetch = Date.now();
-
-    log('Fetching auction data... (this will take a while)');
-    moveUpLines(1);
-
-    const { auctions } = await request({ uri: url, json: true });
-
-    const diffFetch = toHHMMSS((Date.now() - startTimeFetch) / 1000);
-
-    log(`Fetched auction data in ${diffFetch}`);
-
-    const startTimeProcess = Date.now();
-    log(`Processing data...`);
-    moveUpLines(1);
-
-
-    const dataParser = async ([ deckId, deckCards ]) => {
-        const { deckAuction, cardAuctions } = parseAuctionData(auctions, deckId, deckCards);
-
-        const deckCost = Math.ceil(deckAuction.buyout);
-        const cardCost = cardAuctions.reduce((s, c) => s + c, 0);
-        const profit = Math.floor((deckCost - cardCost) * 0.95);
-
-        const deckData = await getItemData(deckId);
-        const profitPercent = Math.floor(profit / cardCost * 100);
-
-        return {
-            name: deckData.name,
-            deck: deckCost,
-            cost: cardCost,
-            profit: profit,
-            profitPercent: Math.abs(profitPercent),
-        };
-    };
-
-    const data =
-        Object.entries(decks)
-              .map(([ deckId, cardIdList ]) => [ Number(deckId), cardIdList ])
-              .map(dataParser);
-
-    const diffProcess = (Date.now() - startTimeProcess);
-
-    log(`Processed data in ${diffProcess}ms`);
-
+async function displayAuctions(auctionPromises) {
     return (
-        Promise.all(data)
+        Promise.all(auctionPromises)
                .then((decks) => decks.filter((deck) => deck))
                .then((decks) => decks.sort((a, b) => b.profitPercent * Math.sign(b.profit) - a.profitPercent * Math.sign(a.profit)))
                .then((decks) => {
@@ -230,15 +223,99 @@ async function doWork(timeout = 0) {
                    log('  ', fmt`Card buyout cost: ${deck.cost}^yg^`);
                    log('  ', fmt`Post-Cut ${profitText} ${Math.abs(deck.profit)}^yg^ (${deck.profitPercent}% of cost)`);
                }))
-               .finally(() => {
-                   const waitTime = 2 * 60 * 1000;
-                   const fnTime = Date.now() - initCallTime;
-
-                   console.log();
-                   doWork(waitTime - fnTime);
-               })
     );
 }
 
+async function waitFor(milliseconds = 0, text = 'Waiting for') {
+    const then = Date.now();
+
+    clearLine();
+    process.stdout.write(fmt`${text} ^+${toHHMMSS(Math.ceil(milliseconds / 1000))} ${milliseconds % 1000}ms^\r`);
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const diff = Date.now() - then;
+
+            resolve(milliseconds - diff);
+        }, 60);
+    });
+}
+
+async function catchError() {
+    /*log('Something went wrong...');
+    console.log();
+
+    const errorText = 'Trying again in';
+    let time = await waitFor(5000, errorText);
+    while (time > 0)
+        time = await waitFor(time, errorText);
+    return doWork().catch(catchError);
+    */
+    log('Something went wrong...');
+    console.log();
+    return doWork(5000).catch(catchError);
+}
+
+const decks = {
+    // Fathoms
+    159125: [
+        153605, 153621, 153622, 153623, 153624, 153625, 153626, 153627,
+    ],
+    // Squalls
+    159126: [
+        153604, 153614, 153615, 153616, 153617, 153618, 153619, 153620,
+    ],
+    // Tides
+    159127: [
+        153603, 153607, 153608, 153609, 153610, 153611, 153612, 153613,
+    ],
+    // Blockades
+    159128: [
+        153606, 153628, 153629, 153630, 153631, 153632, 153633, 153634,
+    ],
+};
+
+async function doWork(timeout = 0) {
+    if (timeout > 0) {
+        return waitFor(timeout)
+            .then(doWork)
+            .catch(catchError);
+    }
+
+    clearLine();
+    moveTo(1, 1);
+    log('Fetching auction data info...');
+    log('');
+    log('');
+    moveUpLines(3);
+
+    const { url, date } = await getAuctionUrl();
+    log(fmt`Latest auction data is ^+${toHHMMSS((moment().utc() - date) / 1000)} old^`);
+
+    const startTime = Date.now();
+    const progressBar = getProgressBar('Downloading auction data...');
+    const rawAuctions = await fetchAuctionData(url, progressBar.update);
+
+    log(fmt`Fetched data in ${toHHMMSS((Date.now() - startTime) / 1000)}^`);
+
+    const startTimeProcess = Date.now();
+    const mapper = formatAuctionData.bind(this, rawAuctions);
+    const auctionPromises =
+        Object.entries(decks)
+              .map(([ deckId, cardIdList ]) => [ Number(deckId), cardIdList ])
+              .map(mapper);
+
+    log(`Processed data in ${Date.now() - startTimeProcess}ms`);
+
+    await displayAuctions(auctionPromises);
+
+    const waitTime = 2 * 60 * 1000;
+    const fnTime = Date.now() - startTime;
+
+    console.log();
+    doWork(waitTime - fnTime);
+}
+
 console.clear();
-doWork();
+doWork()
+    .catch(catchError);
