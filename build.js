0de2803e67e66d2a93b2e7fca5a2ff56dcbb4dc8
Josip Igrec <me@allypost.net>
2018-08-23 20:40:27 +0200

    Now dynamically getting files to build

--- build.js
+++ build.js
@@ -1,27 +1,44 @@
 const { compile } = require('nexe');
 const path = require('path');
+const fs = require('fs');
 
-const files = [
-    'darkmoon-cards',
-    'test',
-];
+async function getFiles() {
+    return new Promise((resolve, reject) => {
+        fs.readdir('./', (err, files) => {
+            if (err)
+                reject(err);
 
-const configs = files.map((file) => ({
-    name: file,
-    input: path.resolve(`${file}.js`),
-    output: path.resolve('build/', file),
-    loglevel: 'silent',
-}));
+            const targetFiles =
+                files
+                    .filter((file) => file.slice(-3) === '.js')
+                    .filter((file) => file !== path.basename(__filename))
+                    .map((file) => file.substr(0, file.length - 3));
 
-const builds = configs.map(async (config) => {
-    console.log(`Building ${config.name}`);
-
-    return compile(config)
-        .then(() => {
-            console.log(`Built ${config.name}`);
+            resolve(targetFiles);
         });
-});
+    });
+}
 
-Promise.all(builds)
-       .then(() => console.log('Done. Finished binaries are in /build/'))
-       .catch((e) => console.log('There were some errors.', '\n', e));
+(async function() {
+    const files = await getFiles();
+
+    const configs = files.map((file) => ({
+        name: file,
+        input: path.resolve(`${file}.js`),
+        output: path.resolve('build/', file),
+        loglevel: 'silent',
+    }));
+
+    const builds = configs.map(async (config) => {
+        console.log(`Building ${config.name}`);
+
+        return compile(config)
+            .then(() => {
+                console.log(`Built ${config.name}`);
+            });
+    });
+
+    Promise.all(builds)
+           .then(() => console.log('Done. Finished binaries are in /build/'))
+           .catch((e) => console.log('There were some errors.', '\n', e));
+})();
